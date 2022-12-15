c0bbc0c02740a5f9e6b53c75562464e0a0c75261
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-12-14 23:23:19 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ function App() {
 
   return (
     <div className="Appsdf">
-      <div>
+      <div>asd
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo resdfdsfact" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
